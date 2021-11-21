454864873f94a4e471b6bd776d2576fdec7813aa
Anton Moskalenko <moskalenko.anton@gmail.com>
2021-11-21 13:08:03 +0200

    Reformat (2/3).

--- Backside/Manager.php
+++ Backside/Manager.php
@@ -14,13 +14,39 @@ use Rune\Application as RuneApplication;
 class Manager
 {
     /**
+     * Configuration manager.
+     *
+     * @var \Rune\Services\Manager
+     */
+    private $config;
+
+    /**
+     * Manager constructor.
+     *
+     * @param array $data Configuration for your site
+     */
+    public function __construct(array $data)
+    {
+        // @todo Add protected method.
+        $this->config = new ConfigurationManager($data);
+    }
+
+    /**
      * Run manager of applications.
      *
      * @return string HTML output
      */
-    public static function run(): string
+    public function run(): string
     {
         $url = $_SERVER['REQUEST_URI'];
+
+        // @todo: assert base application
+
+        $classBaseApplication = $this->config->get('base')['application'];
+        if(class_exists($classBaseApplication)) {
+            $baseApplication = new $classBaseApplication($this->config);
+            return $baseApplication->compile();
+        }
 
         return 'Application with this URL not found.';
     }
